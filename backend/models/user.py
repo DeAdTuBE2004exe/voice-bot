115bc5709e822679940f93e55ce5093812b946cf
DeAdTuBE2004exe <bharatkulkarni.0000@gmail.com>
2025-08-05 23:31:11 +0530

    added created_at when creating users

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,6 +4,7 @@ class User(Document):
     username = StringField(required=True, unique=True, max_length=50)
     email = EmailField(required=True, unique=True)
     password_hash = StringField(required=True)
+    created_at = DateTimeField(default=datetime.datetime.utcnow)
 
     meta = {
         'collection': 'users',
